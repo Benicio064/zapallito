b27828c6fe4681293f2db146387fb81503bd0eb4
Beni <pizarrobenicio671@gmail.com>
2025-08-05 21:46:14 -0300

    añado eventos

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+console.log
+('==========================');
+console.log("Tarea 02 - Eventos");
+console.log
+('===================================');
+
+// Evento onclick
+// este evento se dispara cuando el user hace click en un elemento
+function enArchivo() {
+    alert("Adios, cerraste SESION");
+}
